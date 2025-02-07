97e33ae93ba34c926e2e15023373c9f5f6424864
sergioruizj <sergio.ruizj@edu.uah.es>
2025-02-07 10:31:16 +0100

    Ejercicio9

---
+++ Ejercicio9.py
@@ -0,0 +1,16 @@
+'''Realiza una función recursiva que calcule el siguiente sumatorio:  
+S =  1 + 2 + 3 + 4 + ⋯. + n - 1 + n Realiza un análisis de eficiencia y de complejidad.'''
+
+
+def sumaRec(n: int) -> int:
+    if n == 0:
+        return n
+    else:
+        return n + sumaRec(n-1)
+    
+def main():
+    numero = int(input("Introducza un número entero positivo: "))
+    print(f"La suma de todos los números enteros desde 1 hasta {numero} es {sumaRec(numero)}")
+
+if __name__ == "__main__":
+    main()
