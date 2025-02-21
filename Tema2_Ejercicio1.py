c4bff243f0bf02bf289b51a2ec55bac07f72913c
raquelvegas <raquel.vegas@edu.uah.es>
2025-02-21 11:31:23 +0100

    Ejercicio1

---
+++ Tema2_Ejercicio1.py
@@ -0,0 +1,42 @@
+def mejor_candidatoA(candidatos :list) -> any:
+    """ Devuelve el mejor candidato """
+    pareja = (candidatos[0], candidatos[1])
+    candidatos = candidatos[2:]
+    return pareja, candidatos
+
+def vorazA(candidatos :list) -> list:
+    solucion = []
+    while len(candidatos) > 0:
+        pareja, candidatos = mejor_candidatoA(candidatos)
+        solucion.append(pareja)
+    return solucion
+
+lista = [5,8,1,4,7,9]
+parejas = vorazA(lista)
+mayor = 0
+for pareja in parejas:
+    mayor = max(mayor, pareja[0] + pareja[1])
+print(f"La solución al apartado A es {mayor}")
+
+
+
+def mejor_candidatoB(candidatos :list) -> any:
+    """ Devuelve el mejor candidato """
+    pareja = (candidatos[0], candidatos[-1])
+    candidatos = candidatos[1:-1]
+    return pareja, candidatos
+
+def vorazB(candidatos :list) -> list:
+    solucion = []
+    candidatos = sorted(candidatos)
+    while len(candidatos) > 0:
+        pareja, candidatos = mejor_candidatoB(candidatos)
+        solucion.append(pareja)
+    return solucion
+
+lista = [5,8,1,4,7,9]
+parejas = vorazB(lista)
+mayor = 0
+for pareja in parejas:
+    mayor = max(mayor, pareja[0] + pareja[1])
+print(f"La solución al apartado B es {mayor}")
