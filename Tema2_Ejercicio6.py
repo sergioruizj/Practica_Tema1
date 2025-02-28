8bb4bebff256e7eb48ba4070c73eb933e9755acc
raquelvegas <raquel.vegas@edu.uah.es>
2025-02-28 10:03:51 +0100

    Ejercicio6.2

--- Tema2_Ejercicio6.py
+++ Tema2_Ejercicio6.py
@@ -1,29 +1,35 @@
-'''Dado un conjunto de reservas de pistas de pádel, cada una con una hora de inicio 
-y otra de finalización, diseñe un algoritmo que calcule el número mínimo de pistas 
-necesarias  para  que  todas  las  reservas  puedan  llevarse  a  cabo  sin  conflictos  de  
-horario. Para ello se dispone de una lista de intervalos de tiempo, donde cada intervalo está 
-representado por una hora de inicio y una de finalización de la reserva.'''
+def mejor_candidato(candidatos):
+    """ Devuelve el mejor candidato, el que finaliza antes """
+    return min(candidatos, key=lambda x: x[1])
 
-def mejor_candidato(candidatos: list) -> any:
-    """ Devuelve el mejor candidato """
-    return min(candidatos, key=lambda x: x[1])  # Selecciona el candidato que termina más temprano
+def es_completable(solucion, candidato):
+    """ Comprueba si el candidato puede añadirse sin solapamiento """
+    for reserva in solucion:
+        if not (candidato[0] >= reserva[1] or candidato[1] <= reserva[0]):
+            return False
+    return True
 
-def es_completable(solucion: list, candidato: any) -> bool:
-    """ Comprueba si se puede llegar a la solución usando ese candidato """
-    if not solucion:
-        return True
-    return solucion[-1][1] <= candidato[0]  # No debe solaparse con la última reserva añadida
-
-def voraz(candidatos: list) -> list:
-    solucion = []
-    candidatos.sort(key=lambda x: x[1])  # Ordenamos por hora de finalización
-    while len(candidatos) > 0:
-        c = mejor_candidato(candidatos)
-        candidatos.remove(c)
-        if es_completable(solucion, c):
-            solucion.append(c)
-    return solucion
+def voraz(reservas):
+    """ Calcula el número mínimo de pistas necesarias usando un algoritmo voraz """
+    if not reservas:
+        return 0
+    
+    reservas.sort()  # Ordenamos por hora de inicio
+    pistas = []  # Lista de pistas ocupadas
+    
+    for reserva in reservas:
+        asignado = False
+        for pista in pistas:
+            if es_completable(pista, reserva):
+                pista.append(reserva)
+                asignado = True
+                break
+        
+        if not asignado:
+            pistas.append([reserva])  # Se necesita una nueva pista
+    
+    return len(pistas)
 
 # Ejemplo de uso
-reservas = [(10, 12), (9, 11), (11, 13)]
-print(len(voraz(reservas)))  # Salida: 2
+reservas = [(10, 12), (9, 11), (11, 13), (10, 12)]
+print(voraz(reservas))  # Salida: 3
