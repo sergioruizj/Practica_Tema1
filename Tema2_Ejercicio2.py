f50235275705fcfe1b073ab0fbb2c0c7721b0700
sergioruizj <sergio.ruizj@edu.uah.es>
2025-03-05 11:51:18 +0100

    Ejercicio2

--- Tema2_Ejercicio2.py
+++ Tema2_Ejercicio2.py
@@ -1,12 +1,33 @@
+"""Se tiene que almacenar un conjunto de n ficheros en una cinta magnética (soporte 
+de almacenamiento de recorrido secuencial), donde cada fichero tiene una 
+longitud  conocida  l1,l2,...,ln.  Para  simplificar  el  problema,  puede  suponerse  que  
+la  velocidad  de  lectura  es  constante,  así  como  la  densidad  de  información  en  la  
+cinta. 
+
+Se  conoce  de  antemano  la  tasa  de  utilización  de  cada  fichero  almacenado,  es  
+decir, se sabe la cantidad de peticiones pi correspondiente al fichero i que se van 
+a realizar. Por tanto, el total de peticiones al soporte será la suma total de las peticiones de cada fichero.
+
+Tras la petición de un fichero, al ser encontrado, la cinta se rebobina 
+automáticamente hasta el principio. 
+
+El objetivo es decidir el orden en que los ficheros deben ser almacenados para que 
+se minimice el tiempo medio de carga, creando un algoritmo voraz correcto.
+"""
+
+
+LONGITUD = 0
+PETICIONES = 1
+
 def mejor_candidato(candidatos :list) -> any:
-    return max(candidatos, key=lambda x: x[1] / x[0])
+    return max(candidatos, key=lambda x: x[PETICIONES] / x[LONGITUD])
 
 
-def voraz(candidatos :list) -> list:
+def ordenar_ficheros(ficheros :list) -> list:
     solucion = []
-    while len(candidatos) > 0:
-        c = mejor_candidato(candidatos)
-        candidatos.remove(c)
+    while len(ficheros) > 0:
+        c = mejor_candidato(ficheros)
+        ficheros.remove(c)
         solucion.append(c)
     return solucion
 
@@ -15,11 +36,12 @@ def voraz(candidatos :list) -> list:
 ##############################
 #          TESTS             #
 ##############################
-# Pytest ejecuta las funciones que empiezan por test_
+
+
 def test_orden_ficheros():
     ficheros_entrada = [(10, 100), (20, 50), (5, 200), (15, 30)]
     ficheros_ordenados_esperados = [(5, 200), (10, 100), (20, 50), (15, 30)]  
-    resultado = voraz(ficheros_entrada)
+    resultado = ordenar_ficheros(ficheros_entrada)
 
     assert resultado == ficheros_ordenados_esperados
 
@@ -29,16 +51,17 @@ def test_benchmark_voraz_10(benchmark):
     (1, 2), (2, 3), (3, 4), (4, 5), (5, 6), 
     (6, 7), (7, 8), (8, 9), (9, 10), (10, 11)
     ]
-    resultado = benchmark(voraz, ficheros_entrada)  
+    resultado = benchmark(ordenar_ficheros, ficheros_entrada)  
     assert len(resultado) == len(ficheros_entrada)   
+
 
 def test_benchmark_voraz_100(benchmark):
     ficheros_entrada = [(i, i % 100 + 1) for i in range(1, 101)]  
-    resultado = benchmark(voraz, ficheros_entrada)  
+    resultado = benchmark(ordenar_ficheros, ficheros_entrada)  
     assert len(resultado) == len(ficheros_entrada)  
 
-# Test 3: Benchmark para 500 ficheros
+
 def test_benchmark_voraz_500(benchmark):
-    ficheros_entrada = [(i, i % 100 + 1) for i in range(1, 501)]  # 500 ficheros
-    resultado = benchmark(voraz, ficheros_entrada)  # Medir tiempo de ejecución
-    assert len(resultado) == len(ficheros_entrada)  # Comprobar que procesó todos los ficheros
+    ficheros_entrada = [(i, i % 100 + 1) for i in range(1, 501)]  
+    resultado = benchmark(ordenar_ficheros, ficheros_entrada)  
+    assert len(resultado) == len(ficheros_entrada) 
