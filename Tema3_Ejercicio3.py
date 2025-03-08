b6572e47711229c126ba887e85ede794c852dfea
sergioruizj <sergio.ruizj@edu.uah.es>
2025-03-08 19:51:43 +0100

    Cambios VI (sigue faltando hacer bien el ejercicio 3 del tema 3)

--- Tema3_Ejercicio3.py
+++ Tema3_Ejercicio3.py
@@ -10,36 +10,103 @@ algoritmo  serán  el  intervalo  [p1, p2],  el  valor k que  se  está  buscand
 para la aproximación.'''
 
 
-def buscarValores(f: callable, x0: float, x1: float, k: float, epsilon: float) -> list:
+def buscarValores(f: callable, p1: float, p2: float, k: float, epsilon: float) -> list:
     def buscar(a, b) -> list:
-        if (abs((b - a) < epsilon)):
-           return [[a, b]]
-            
+        if abs(b - a) < epsilon:
+            if f(a) <= k <= f(b) or f(a) >= k >= f(b):
+                return [[a, b]]  
+            else: 
+                return []
         
         mid = (a + b) / 2
         f_mid = f(mid)
-
-        izq = []
-        dch = []
-
-        if ((f(a) <= k <= f_mid) or (f(a) >= k >= f_mid)):
+        
+        izq, dch = [], []
+        
+        if (f(a) <= k <= f_mid) or (f(a) >= k >= f_mid):
             izq = buscar(a, mid)
-        if ((f_mid <= k <= f(b)) or (f_mid >= k >= f(b))):
+        if (f_mid <= k <= f(b)) or (f_mid >= k >= f(b)):
             dch = buscar(mid, b)
 
+        ''' Hasta ahora el método sigue buscando aunque se cumpla la condición inicial, a continuación llevamos 
+        a cabo un procedimiento para unificar intervalos que sean muy cercanos entre sí '''
+
+        # Unificación de intervalos
+        if izq and dch and abs(izq[-1][1] - dch[0][0]) < epsilon:
+            return izq[:-1] + [[izq[-1][0], dch[0][1]]] + dch[1:]
+        
         return izq + dch
     
-    return buscar(x0, x1)
+    return buscar(p1, p2)
 
 def cuadrado(x):
     return x**2
 
-def main(): 
-    print(buscarValores(cuadrado, -2, 2, 1, 0.0001))
+def main():
+    punto = 1
+    x0 = -2
+    x1 = 2
+    resultado = buscarValores(cuadrado, x0, x1, punto, 0.0001)
+    print(f'La función f(x) = x^2, en el intervalo [{x0}, {x1}] toma valores igual a {punto} en dos puntos comprendidos en los intervalos ({resultado[0][0]}, {resultado[0][1]}), ({resultado[1][0]}, {resultado[1][1]})')
 
 if __name__ == "__main__":
+    def _func1(x: float) -> float:
+        return x**3 - 4*x + 4
+    
+    eps1 = 0.001
+    x10 = 0
+    x11 = 6
+    k1 = 3.9
+
+    resultado1 = buscarValores(_func1, x10, x11, k1, eps1)
+    print(resultado1)
+
     main()
+
 
 ##############################
 #          TESTS             #
 ##############################
+
+def test_buscarValores():
+    import math
+
+    def _cuadrado(x):
+        return x**2
+    
+    def _seno(x):
+        return math.sin(x)
+    
+    eps1 = 0.001
+    cuadrado = buscarValores(_cuadrado, -2, 2, 1, eps1)
+    
+    assert len(cuadrado) == 2  # Comprobamos que hay dos puntos que toman ese valor
+    assert cuadrado[0][0] <= -1 <= cuadrado[0][1]  # Comprobamos que el mínimo esta en el intervalo
+    assert abs(cuadrado[0][0] - cuadrado[0][1]) <= 2 * eps1  # Comprobamos que el intervalo tenga longitud 2 epsilon
+    
+    seno = buscarValores(_seno, 14, 15, 0.9, eps1)
+    
+    assert len(seno) == 1  # Comprobamos que hay dos puntos que toman ese valor
+    assert abs(seno[0][0] - seno[0][1]) <= 2 * eps1  # Comprobamos que el intervalo tenga longitud 2 epsilon
+    
+
+def test_benchmark_buscarValores():
+    import Tests_timer
+    import math
+    
+    @Tests_timer.benchmark
+    def _timer_buscarValores(func: callable, x0: float, x1: float, k: float, epsilon: float) -> tuple:
+        return buscarValores(func, x0, x1, k, epsilon)
+    
+    Tests_timer.warmup()
+    print()
+
+    def _func1(x: float) -> float:
+        return x**3 - 4*x
+    
+    eps1 = 0.001
+    x10 = 0
+    x11 = 6
+    k1 = -2
+
+    resultado1 = _timer_buscarValores(_func1, x10, x11, k1, eps1)
